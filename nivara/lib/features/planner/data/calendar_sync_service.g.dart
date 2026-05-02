825a36ef0ea298fed91b8c38aff5d099495888ad
upkar.nz@gmail.com <Upinder@23>
2026-05-03 11:37:34 +1200

    feat(planner): add CalendarSyncService for bi-directional GCal sync

---
+++ nivara/lib/features/planner/data/calendar_sync_service.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'calendar_sync_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$calendarSyncServiceHash() =>
+    r'a1d0905f38284fb32459857d2230e24b71f0874c';
+
+/// See also [calendarSyncService].
+@ProviderFor(calendarSyncService)
+final calendarSyncServiceProvider =
+    AutoDisposeProvider<CalendarSyncService>.internal(
+  calendarSyncService,
+  name: r'calendarSyncServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$calendarSyncServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CalendarSyncServiceRef = AutoDisposeProviderRef<CalendarSyncService>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
